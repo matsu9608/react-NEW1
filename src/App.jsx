e1d7f6e9c5273994905fd9c0dd8361093827cb51
matsu9608 <92790736+matsu9608@users.noreply.github.com>
2022-02-19 11:40:48 +0900

    react-tuika

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useState, useEffect } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 //コンポーネント
 const App = () => {
-  const onClickButton = () => alert();
+  //state一覧
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  //カウント処理
+  const onClickCountButton = () => {
+    setNum(num + 1);
+    console.log(num);
+  };
+
+  const onClickfaceShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColorfulMessage />
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">電気ですよ</ColorfulMessage>
+      {/* <ColorfulMessage color="pink" message="お元気です" /> */}
       <p style={{ color: "red" }}>こんにちは</p>
-      <button onClick={onClickButton}>こんにちは</button>
+      <button onClick={onClickCountButton}>カウントアップ</button>
+      <p>{num}</p>
+      <button onClick={onClickfaceShowFlag}>on/off</button>
+      {faceShowFlag ? <p>( ^ω^ )</p> : <p>( )</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import App from "../App";
 
-const ColorfulMessage = () => {
+//コンポーネントの引数にpropsを設定
+export const ColorfulMessage = (props) => {
+  //毎回propsを指定しなくて済むように分割代入を行う
+  const { children, color } = props;
+
   const contentStyle = {
-    color: "blue",
+    color: color,
     fontSize: "18px"
   };
   return (
     <>
-      <p style={contentStyle}>こんにちは</p>
+      <p style={contentStyle}>{children}</p>
     </>
   );
 };
-
-export default ColorfulMessage;
